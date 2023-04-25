c2656947724a6687ec1f2a432cda3682698524bd
Zuly <zulyhtet77@gmail.com>
2023-04-25 19:12:37 +0630

    finished profile

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-include ('vendor/autoload.php');
+// include ('vendor/autoload.php');
 
-use Carbon\Carbon;
-use App\Library\Math;
+// use Carbon\Carbon;
+// use App\Library\Math;
 
-echo Carbon::now()->addDay();
-echo Math::add(1, 3);
+// echo Carbon::now()->addDay();
+// echo Math::add(1, 3);
